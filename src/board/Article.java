43b01015ffab57dd459e5b90843f89b9331f42e9
donhyuck <leedonhyuck123@gmail.com>
2021-12-12 09:03:20 +0900

    명령어 입력받기

--- src/board/Article.java
+++ src/board/Article.java
@@ -1,7 +1,14 @@
 package board;
 
+import java.util.Scanner;
+
 public class Article {
 	public static void main(String[] args) {
 		System.out.println("==프로그램 시작==");
+		Scanner input = new Scanner(System.in);
+
+		String command = input.next();
+		System.out.println(command);
+
 	}
 }
